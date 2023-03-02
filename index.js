7281bd54e6b18062277adbcb1780c3dbc6c56be6
Krintox <shashanksuggala@gmail.com>
2023-03-02 16:14:58 +0530

    Removed Routes

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ app.get("/", (req, res) => {
   res.render("index", { core: coredata });
 });
 
-app.get("/codeasymptote", (req, res) => {
-  res.render("events", { core: coredata });
-});
+// app.get("/codeasymptote", (req, res) => {
+//   res.render("events", { core: coredata });
+// });
 
-app.get("/mlbootcamp", (req, res) => {
-  res.render("events", { core: coredata });
-});
+// app.get("/mlbootcamp", (req, res) => {
+//   res.render("events", { core: coredata });
+// });
 
-app.get("/webdev", (req, res) => {
-  res.render("events", { core: coredata });
-});
+// app.get("/webdev", (req, res) => {
+//   res.render("events", { core: coredata });
+// });
 
-app.get("/electronix", (req, res) => {
-  res.render("events", { core: coredata });
-});
+// app.get("/electronix", (req, res) => {
+//   res.render("events", { core: coredata });
+// });
 
-app.get("/technica", (req, res) => {
-  res.render("technica");
-});
+// app.get("/technica", (req, res) => {
+//   res.render("technica");
+// });
 
 app.listen(7000, () => {
   console.log(`Server listening on port 7000`);
